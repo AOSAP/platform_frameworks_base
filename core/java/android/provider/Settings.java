e610335f7d60baa22fa2b53ae28c3a0ddfa17f1b
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    @neobuddy89: Fixes and simplify.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    Change-Id: I523fc1247b912915a9200d4ed9e790b3301c0e2a
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5717,6 +5717,12 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
