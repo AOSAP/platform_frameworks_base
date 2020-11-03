omnirom/android_frameworks_base_old

[1/2] SystemUI: Add custom color to ambient pulse notification lights
Add custom color to ambient pulse notification lights

Change-Id: Ie67310221c0a100497a9d36847915d52e3854de1

core/java/android/provider/Settings.java
@@ -5350,12 +5350,6 @@ public final class Settings {
          */
         public static final String OMNI_AOD_NOTIFICATION_PULSE_ACTIVATED = "ambient_notification_light_activated";
 
-         /**
-         * Whether to use accent color for pulse
-         * @hide
-         */
-        public static final String OMNI_NOTIFICATION_PULSE_ACCENT = "ambient_notification_light_accent";
-
         /**
          * helper setting to contain the current pulse reason so we can check
          * if it has been triggered from a notification or a gesture
@@ -5368,6 +5362,18 @@ public final class Settings {
          * @hide
          */
         public static final String OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
+
+         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String OMNI_NOTIFICATION_PULSE_ACCENT = "ambient_notification_light_accent";
+
+         /**
+         * Pulse Ambient lights color
+         * @hide
+         */
+        public static final String OMNI_NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
 
         /**
          * SettingsBackupAgent will combine its list with this so we dont need
@@ -5395,6 +5401,7 @@ public final class Settings {
             OMNI_AOD_NOTIFICATION_PULSE_CLEAR,
             OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT,
             OMNI_NOTIFICATION_PULSE_ACCENT,
+            OMNI_NOTIFICATION_PULSE_COLOR,
         };
 
         /**
@@ -5429,6 +5436,7 @@ public final class Settings {
             OMNI_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_CLEAR, 0);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT, 1);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE_ACCENT, 0);
+            OMNI_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE_COLOR, 1);
         }
     }
 

AOSAP/platform_frameworks_base

Signed-off-by: DennySPb <dennyspb@gmail.com>

core/java/android/provider/Settings.java
@@ -5448,12 +5448,6 @@ public final class Settings {
          */
         public static final String AOD_NOTIFICATION_PULSE_ACTIVATED = "ambient_notification_light_activated";
 
-         /**
-         * Whether to use accent color for pulse
-         * @hide
-         */
-        public static final String NOTIFICATION_PULSE_ACCENT = "ambient_notification_light_accent";
-
         /**
          * helper setting to contain the current pulse reason so we can check
          * if it has been triggered from a notification or a gesture
@@ -5466,6 +5460,18 @@ public final class Settings {
          * @hide
          */
         public static final String AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
+
+         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_ACCENT = "ambient_notification_light_accent";
+
+         /**
+         * Pulse Ambient lights color
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to