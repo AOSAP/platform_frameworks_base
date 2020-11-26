omnirom/android_frameworks_base_old

SystemUI: add timeout for ambient notification pulse
Change-Id: Ifb2a7f4e14711b522fbfe8b9c546ea78c48af268

core/java/android/provider/Settings.java
@@ -5332,7 +5332,6 @@ public final class Settings {
          */
         public static final String OMNI_AOD_NOTIFICATION_PULSE = "ambient_notification_light_enabled";
 
-
          /**
          * Whether hide everything on aod when  ambient lights are enabled
          * @hide
@@ -5365,6 +5364,12 @@ public final class Settings {
         public static final String OMNI_PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
         /**
+         * Timeout for ambient pulse in seconds - 0 is no timeout
+         * @hide
+         */
+        public static final String OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
+
+        /**
          * SettingsBackupAgent will combine its list with this so we dont need
          * to add new things into SettingsProvider SystemSettings
          * @hide
@@ -5388,6 +5393,7 @@ public final class Settings {
             OMNI_NOTIFICATION_PULSE,
             OMNI_AOD_NOTIFICATION_PULSE,
             OMNI_AOD_NOTIFICATION_PULSE_CLEAR,
+            OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT,
             OMNI_NOTIFICATION_PULSE_ACCENT,
         };
 
@@ -5421,6 +5427,7 @@ public final class Settings {
             OMNI_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE, 0);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE, 0);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_CLEAR, 0);
+            OMNI_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT, 1);
             OMNI_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE_ACCENT, 0);
         }
     }

AOSAP/platform_frameworks_base

Signed-off-by: mracar07 <umutcan@umutcanacar.me>
Change-Id: I722ad853db15c47a0f5c92a1a08f04e4c21f1c22

core/java/android/provider/Settings.java
@@ -5884,6 +5884,12 @@ public final class Settings {
         public static final String OMNI_PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
         /**
+         * Timeout for ambient pulse in seconds - 0 is no timeout
+         * @hide
+         */
+        public static final String OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6411,6 +6417,7 @@ public final class Settings {
             OMNI_NOTIFICATION_PULSE,
             OMNI_AOD_NOTIFICATION_PULSE,
             OMNI_AOD_NOTIFICATION_PULSE_CLEAR,
+            OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT,
             OMNI_NOTIFICATION_PULSE_ACCENT,
         };
 
@@ -6428,6 +6435,7 @@ public final class Settings {
             AOSAP_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE, 0);
             AOSAP_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE, 0);
             AOSAP_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_CLEAR, 0);
+            AOSAP_SETTINGS_VALIDATORS.put(OMNI_AOD_NOTIFICATION_PULSE_TIMEOUT, 1);
             AOSAP_SETTINGS_VALIDATORS.put(OMNI_NOTIFICATION_PULSE_ACCENT, 0);
         }
     }